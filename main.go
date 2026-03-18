6d3c31aba4f2551a9e19236b3d04ffa546b1aa6d
arun1500 <114504679+arun1500@users.noreply.github.com>
2026-03-18 12:57:49 +0530

    Fix syntax errors in main.go

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
+
 import (
 	"fmt"
 	"net/http"
 )
 
-func handler (w hhtp.ResponseWriter , r*http.request.Request)
-{
-	fmt.Fprint (w, "Hello Devops World ")
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello DevOps World")
 }
 
-func main()
-{
+func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080, nill")
+	http.ListenAndServe(":8080", nil)
 }
